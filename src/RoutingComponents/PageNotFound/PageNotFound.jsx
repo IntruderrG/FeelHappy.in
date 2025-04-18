13fa677b2da36c01f964d2f09751a23e32ef3596
Gauravendra <gdevtripathi@gmail.com>
2025-04-18 09:34:11 +0530

    Commit-2

--- src/RoutingComponents/PageNotFound/PageNotFound.jsx
+++ src/RoutingComponents/PageNotFound/PageNotFound.jsx
@@ -1,4 +1,4 @@
-import logo from "C:/Users/gdevt/OneDrive/Desktop/HAPPY/Client/Images/PageNotFound/404-Page-Featured-Image.png";
+import logo from "./404-Page-Featured-Image.png";
 function Pagenotfound() {
   return (
     <div className="h-[100vh] flex justify-center items-center">
